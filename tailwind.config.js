9d7907a7ccc6aa6aaa851009e918083550293626
DominickVale <dominickveil@gmail.com>
2024-02-08 16:53:01 +0100

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        layout: "1fr repeat(10, 7rem) 1fr",
+      },
       colors: {
+        primary: "#FF3D00",
         background: {
           dark: "#111010",
         },
