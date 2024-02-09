9d054b34c07d28712266bb235ed41c8d6937b7fe
DominickVale <dominickveil@gmail.com>
2024-02-09 17:26:33 +0100

    more wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,22 @@ export default {
     extend: {
       gridTemplateColumns: {
         // Simple 16 column grid
-        layout: "1fr repeat(10, 7rem) 1fr",
+        layout: "1fr repeat(10, minmax(5rem, 7rem)) 1fr",
       },
       colors: {
         primary: "#FF3D00",
+        inactive: "#a7a7a7",
+        success: "#96D800",
         background: {
           dark: "#111010",
         },
       },
       fontFamily: {
-        sans: ['Inconsolata'],
+        sans: ['Roboto'],
         serif: ['Inconsolata'],
         display: ['Orbitron'],
         body: ['Inconsolata'],
+        mono: ['Inconsolata', 'Roboto Mono']
       },
     },
   },
