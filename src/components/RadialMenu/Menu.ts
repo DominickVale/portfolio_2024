6a70bd6bbcf865f82bf9f5b8cb31ee928d9e28c8
DominickVale <dominickveil@gmail.com>
2024-03-31 12:07:31 +0200

    working thumb

--- src/components/RadialMenu/Menu.ts
+++ src/components/RadialMenu/Menu.ts
@@ -8,7 +8,7 @@ type RadialMenuOptions = {
   position?: Vec2
 }
 
-const SVGNS = "http://www.w3.org/2000/svg";
+const SVGNS = 'http://www.w3.org/2000/svg'
 const bgColor = 'var(--bg-radial-02)'
 
 export default class Menu {
@@ -66,59 +66,66 @@ export default class Menu {
     this._wrapperBounds = this._wrapper.getBoundingClientRect()
     this._thumbBounds = this._thumb.getBoundingClientRect()
 
-    this._wrapper.style.setProperty('--pos-x', this._position.x + "px")
-    this._wrapper.style.setProperty('--pos-y', this._position.y + "px")
+    this._wrapper.style.setProperty('--pos-x', this._position.x + 'px')
+    this._wrapper.style.setProperty('--pos-y', this._position.y + 'px')
 
     this._radius = menuBounds.width / 2
-    this.innerRadius = this._radius * this.innerRadiusPercent / 100
+    this.innerRadius = (this._radius * this.innerRadiusPercent) / 100
     this._centralAngle = 360 / this.items.length
   }
 
   create() {
-    const innerRadius = this._radius * this.innerRadiusPercent / 100
+    const innerRadius = (this._radius * this.innerRadiusPercent) / 100
     const center = this._radius
     const gapRad = degToRad(this.gap / 10)
     const sliceAngle = degToRad(this._centralAngle)
     const maskPathData: string[] = []
 
-    this._shape.setAttribute('viewBox', `0 0 ${this._radius * 2} ${this._radius * 2}`)
+    this._shape.setAttribute(
+      'viewBox',
+      `0 0 ${this._radius * 2} ${this._radius * 2}`,
+    )
     this.items.forEach((_, i) => {
-      const startAngle = ( i * sliceAngle) + TAU / 4
+      const startAngle = i * sliceAngle + TAU / 4
       const endAngle = startAngle + sliceAngle
 
       const menuItemAngleRad = startAngle + sliceAngle / 2
-      const menuItemPosFactor = (this._radius / 2) + (this._radius * this.innerRadiusPercent / 100) / 2
+      const menuItemPosFactor =
+        this._radius / 2 + (this._radius * this.innerRadiusPercent) / 100 / 2
       const menuItemPos = {
-        x:
-          Math.cos(menuItemAngleRad) * menuItemPosFactor + this._radius,
-        y:
-         Math.sin(menuItemAngleRad) * menuItemPosFactor + this._radius,
+        x: Math.cos(menuItemAngleRad) * menuItemPosFactor + this._radius,
+        y: Math.sin(menuItemAngleRad) * menuItemPosFactor + this._radius,
       }
 
-      const menuItemEl = $all('.radial-menu-item', this._wrapper)[i] as HTMLElement
+      const menuItemEl = $all('.radial-menu-item', this._wrapper)[
+        i
+      ] as HTMLElement
       menuItemEl.style.setProperty('--x', menuItemPos.x + 'px')
       menuItemEl.style.setProperty('--y', menuItemPos.y + 'px')
       menuItemEl.setAttribute('data-i', i.toString())
-
+      menuItemEl.setAttribute('data-x', menuItemPos.x.toString())
+      menuItemEl.setAttribute('data-y', menuItemPos.y.toString())
 
       const existingSlice = $(`[data-i="${i}"]`, this._shape)
-      const slice = existingSlice || document.createElementNS(SVGNS, "path");
+      const slice = existingSlice || document.createElementNS(SVGNS, 'path')
       slice.setAttribute('fill', bgColor)
       slice.setAttribute('data-i', i.toString())
 
       const radiusesRatio = this._radius / innerRadius
-      const innerGapRad = degToRad(( this.gap / 10 ) * radiusesRatio )
-      const outerStartX = center + (this._radius * Math.cos(startAngle + gapRad))
-      const outerStartY = center + (this._radius * Math.sin(startAngle + gapRad))
-      const outerEndX = center + (this._radius * Math.cos(endAngle - gapRad))
-      const outerEndY = center + (this._radius * Math.sin(endAngle - gapRad))
+      const innerGapRad = degToRad((this.gap / 10) * radiusesRatio)
+      const outerStartX = center + this._radius * Math.cos(startAngle + gapRad)
+      const outerStartY = center + this._radius * Math.sin(startAngle + gapRad)
+      const outerEndX = center + this._radius * Math.cos(endAngle - gapRad)
+      const outerEndY = center + this._radius * Math.sin(endAngle - gapRad)
 
-      const innerStartX = center + (innerRadius * Math.cos(startAngle + innerGapRad))
-      const innerStartY = center + (innerRadius * Math.sin(startAngle + innerGapRad))
-      const innerEndX = center + (innerRadius * Math.cos(endAngle - innerGapRad))
-      const innerEndY = center + (innerRadius * Math.sin(endAngle - innerGapRad))
+      const innerStartX =
+        center + innerRadius * Math.cos(startAngle + innerGapRad)
+      const innerStartY =
+        center + innerRadius * Math.sin(startAngle + innerGapRad)
+      const innerEndX = center + innerRadius * Math.cos(endAngle - innerGapRad)
+      const innerEndY = center + innerRadius * Math.sin(endAngle - innerGapRad)
 
-      const pathData = `M ${outerStartX } ${outerStartY}
+      const pathData = `M ${outerStartX} ${outerStartY}
                       A ${this._radius} ${this._radius} 0 0 1 ${outerEndX} ${outerEndY}
                       L ${innerEndX} ${innerEndY}
                       A ${innerRadius} ${innerRadius} 0 0 0 ${innerStartX} ${innerStartY}
@@ -135,23 +142,23 @@ export default class Menu {
     })
 
     // MASK
-    const maskPath = maskPathData.join(" ")
+    const maskPath = maskPathData.join(' ')
 
     const defs = $('defs', this._shape)
     const mask = $('mask', defs)
-    const maskId =  `radial-menu-mask-${this.id}`
+    const maskId = `radial-menu-mask-${this.id}`
     mask.setAttribute('id', maskId)
-    mask.setAttribute("viewBox", `0 0 ${this._radius * 2} ${this._radius * 2}`);
+    mask.setAttribute('viewBox', `0 0 ${this._radius * 2} ${this._radius * 2}`)
 
     const existingGapPath = $(`path`, mask)
-    const gapPath = existingGapPath || document.createElementNS(SVGNS, "path");
-    gapPath.setAttribute("fill", "white");
-    gapPath.setAttribute("d", maskPath);
-    mask.appendChild(gapPath);
-    defs.appendChild(mask);
-    this._shape.appendChild(defs);
+    const gapPath = existingGapPath || document.createElementNS(SVGNS, 'path')
+    gapPath.setAttribute('fill', 'white')
+    gapPath.setAttribute('d', maskPath)
+    mask.appendChild(gapPath)
+    defs.appendChild(mask)
+    this._shape.appendChild(defs)
     this._bg.style.setProperty('--mask', `url(#${maskId})`)
-  } 
+  }
 
   set size(size: string) {
     this._wrapper.style.setProperty('--size', size)
@@ -166,41 +173,72 @@ export default class Menu {
   }
 
   onSliceClick(ev: MouseEvent) {
-    console.log("CLICK! Add sound here")
+    console.log('CLICK! Add sound here')
   }
   onSliceMouseEnter(ev: MouseEvent) {
-    const slice = ev.currentTarget as SVGElement;
+    const slice = ev.currentTarget as SVGElement
     const i = slice.getAttribute('data-i')
     const item = this.items[i]
     item.setAttribute('data-hover', 'true')
   }
   onSliceMouseLeave(ev: MouseEvent) {
-    const slice = ev.currentTarget as SVGElement;
+    const slice = ev.currentTarget as SVGElement
     const i = slice.getAttribute('data-i')
     const item = this.items[i]
     item.setAttribute('data-hover', 'false')
   }
 
   /*
-    * @TODO: bigger thumb, progress as user moves mouse towards direction, select nearest slice to thumb
-    */
+   * @TODO: bigger thumb, progress as user moves mouse towards direction 
+   */
   handleThumb(ev: MouseEvent) {
-    const x = ev.clientX;
-    const y = ev.clientY;
+    const x = ev.clientX
+    const y = ev.clientY
 
     const rel = {
       x: x - this._position.x,
       y: y - this._position.y,
-    };
+    }
 
-    const distance = Math.sqrt(rel.x ** 2 + rel.y ** 2);
+    const distance = Math.sqrt(rel.x ** 2 + rel.y ** 2)
+    const innerRadiusBound = this.innerRadius - this._thumbBounds.width / 2
 
-    const clampedDistance = clamp(distance, 0, this.innerRadius - (this._thumbBounds.width / 2))
+    const clampedDistance = clamp(distance, 0, innerRadiusBound)
+    const newX = (this._radius + (rel.x / distance) * clampedDistance) || this._radius
+    const newY = (this._radius + (rel.y / distance) * clampedDistance) || this._radius
 
-    const newX = (rel.x / distance) * clampedDistance;
-    const newY = (rel.y / distance) * clampedDistance;
+    let closest = {
+      id: -1,
+      distance: Infinity,
+    }
+    if (clampedDistance >= innerRadiusBound) {
+      this.items.forEach((item, i) => {
+        const itemPos = {
+          x: Number(item.getAttribute('data-x')),
+          y: Number(item.getAttribute('data-y')),
+        }
+        const distanceFromItem = Math.sqrt(
+          (itemPos.x - newX) ** 2 + (itemPos.y - newY) ** 2,
+        )
+        if (distanceFromItem < closest.distance) {
+          closest = {
+            id: i,
+            distance: distanceFromItem,
+          }
+        }
+        item.setAttribute('data-highlighted', 'false')
+        this._shape.children[i].setAttribute('data-highlighted', 'false')
+      })
+      this.items[closest.id].setAttribute('data-highlighted', 'true')
+      this._shape.children[closest.id].setAttribute('data-highlighted', 'true')
+    } else {
+      this.items.forEach((item, i) => {
+        item.setAttribute('data-highlighted', 'false')
+        this._shape.children[i].setAttribute('data-highlighted', 'false')
+      })
+    }
 
-    this._thumb.style.setProperty('--x', this._radius + newX + 'px');
-    this._thumb.style.setProperty('--y', this._radius + newY + 'px');
+    this._thumb.style.setProperty('--x', newX + 'px')
+    this._thumb.style.setProperty('--y', newY + 'px')
   }
 }
