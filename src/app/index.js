56f5d01f5ef08aa1f23152a21db400f537cde677
DominickVale <dominickveil@gmail.com>
2024-08-26 17:24:43 +0200

    better motd

--- src/app/index.js
+++ src/app/index.js
@@ -3,21 +3,37 @@ import gsap from 'gsap'
 import { ScrollTrigger, TextPlugin } from 'gsap/all'
 
 gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(TextPlugin)
 new App()
 
 function fancyMotd() {
-    const styles = [
-    'color: #FFFFFF',
-    'background-color: #000000',
-    'font-family: monospace',
-    'font-size: 14px',
-    'font-weight: bold',
-    'padding: 10px 20px',
-    'border: 1px solid #FFFFFF'
-  ].join(';');
+  const styles = `
+  color: #FFFFFF;
+  background-color: #000000;
+  font-family: monospace;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 10px 20px;
+  border: 1px solid #FFFFFF;
+`
 
-  console.log('%cWelcome, please enjoy your stay 😸', styles);
+  const smallerStyles = `
+  color: #FFFFFF;
+  background-color: #000000;
+  font-family: monospace;
+  font-size: 10px;
+  font-weight: bold;
+  padding: 8px 16px;
+  border: 1px solid #FFFFFF;
+`
+
+  console.log('\n\n\n')
+  console.log('%cWelcome, please enjoy your stay 😸', styles)
+  console.log('%cSound design: Domenico Vale ⋅ Music: Scott Buckley', smallerStyles)
+  console.log(`
+'Shadows and Dust' by Scott Buckley - released under CC-BY 4.0. https://www.scottbuckley.com.au
+`)
+  console.log('\n\n\n')
 }
 
 ;(() => {
